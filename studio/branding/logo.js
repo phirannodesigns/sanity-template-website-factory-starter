2b31d4f04e782ec0cb3d3beb0626a0d7accbb9ae
Luke Bennett <hello@lukebennett.com.au>
2021-04-19 11:42:34 +1000

    Add ESLint comment

--- studio/branding/logo.js
+++ studio/branding/logo.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled, { css } from 'styled-components'
 
+// eslint-disable-next-line react/prop-types
 const Logo = ({ projectName }) => {
   return (
     <Icon isLogin={projectName}>
