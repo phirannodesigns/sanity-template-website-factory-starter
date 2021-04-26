577f19f4434710d5f40e0866ac054d4a65e8413e
Luke Bennett <hello@lukebennett.com.au>
2021-04-26 16:03:41 +1000

    Formatting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,9 +7,9 @@
  */
 
 import './src/styles/global.css';
-import "@fontsource/montserrat/400.css"
-import "@fontsource/montserrat/400-italic.css"
-import "@fontsource/montserrat/500.css"
-import "@fontsource/montserrat/500-italic.css"
-import "@fontsource/montserrat/600.css"
-import "@fontsource/montserrat/600-italic.css"
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/400-italic.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/500-italic.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/600-italic.css';
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         },
         type: {
           DEFAULT: '#ffffff',
-        }
+        },
       },
       fontFamily: {
         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
